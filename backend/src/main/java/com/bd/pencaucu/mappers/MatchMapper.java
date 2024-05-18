3d5e7907d36e98b4d08b470da692586d451ca4cb
ikken <pierosaucedo3@gmail.com>
2024-05-18 19:11:43 -0300

    Implement mappers

--- backend/src/main/java/com/bd/pencaucu/mappers/MatchMapper.java
+++ backend/src/main/java/com/bd/pencaucu/mappers/MatchMapper.java
@@ -1,4 +1,37 @@
 package com.bd.pencaucu.mappers;
 
-public class MatchMapper {
+import com.bd.pencaucu.domain.models.Match;
+import com.bd.pencaucu.domain.models.Team;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.Blob;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MatchMapper implements RowMapper<Match> {
+
+    @Override
+    public Match mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Match match = new Match();
+        String firstTeamName = rs.getString("first_team");
+        String secondTeamName = rs.getString("second_team");
+        Blob firstTeamPicture = rs.getBlob("first_team");
+        Blob secondTeamPicture = rs.getBlob("second_team");
+
+        Team team1 = new Team();
+        team1.setName(firstTeamName);
+        team1.setPicture(firstTeamPicture);
+
+        Team team2 = new Team();
+        team2.setName(secondTeamName);
+        team2.setPicture(secondTeamPicture);
+
+        match.setFirstTeam(team1);
+        match.setSecondTeam(team2);
+        match.setFirstTeamScore(rs.getInt("first_team_score"));
+        match.setSecondTeamScore(rs.getInt("second_team_score"));
+        match.setDate(rs.getDate("date"));
+
+        return match;
+    }
 }
--- backend/src/main/java/com/bd/pencaucu/mappers/UserMapper.java
+++ backend/src/main/java/com/bd/pencaucu/mappers/UserMapper.java
@@ -1,4 +1,19 @@
 package com.bd.pencaucu.mappers;
 
-public class UserMapper {
+import com.bd.pencaucu.domain.models.User;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserMapper implements RowMapper<User> {
+
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        User user = new User();
+        user.setName(rs.getString("name"));
+        user.setEmail(rs.getString("email"));
+        user.setRol(rs.getInt("rol"));
+        return user;
+    }
 }
