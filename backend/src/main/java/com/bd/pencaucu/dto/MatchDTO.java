25e227ea9a8155a66ed371f11b2556f181ebc188
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2024-06-14 16:26:59 -0300

    dummy data insert to db | DTO folder created

---
+++ backend/src/main/java/com/bd/pencaucu/dto/MatchDTO.java
@@ -0,0 +1,14 @@
+package com.bd.pencaucu.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class MatchDTO implements Serializable {
+
+}
