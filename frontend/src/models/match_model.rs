1e6345a52aec924e97f8ad45da24d06978398200
ikken <pierosaucedo3@gmail.com>
2024-06-15 18:41:06 -0300

    Update match_model, fields now matching API response json format

--- frontend/src/models/match_model.rs
+++ frontend/src/models/match_model.rs
@@ -3,9 +3,29 @@ use serde::{Serialize, Deserialize};
 #[derive(Default, Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Match {
     pub(crate) id: u32,
-    pub(crate) date: String,
+    pub(crate) date: u64,
+
+    #[serde(rename = "knockoutStage")]
+    pub(crate) knockout_stage: Option<String>,
+
+    #[serde(rename = "stadiumName")]
+    pub(crate) stadium_name: String,
+
+    #[serde(rename = "teamName")]
     pub(crate) first_team_name: String,
+
+    #[serde(rename = "facedTeamName")]
     pub(crate) second_team_name: String,
-    pub(crate) first_team_score: String,
-    pub(crate) second_team_score: String,
+
+    #[serde(rename = "teamScore")]
+    pub(crate) first_team_score: u32,
+
+    #[serde(rename = "facedTeamScore")]
+    pub(crate) second_team_score: u32,
+
+    #[serde(rename = "teamPictureUrl")]
+    pub(crate) team_picture_url: String,
+
+    #[serde(rename = "facedTeamPictureUrl")]
+    pub(crate) faced_team_picture_url: String,
 }
