0cb90fac1d79c2736b3e86b85d515966efe0b845
ikken <pierosaucedo3@gmail.com>
2024-06-14 22:47:02 -0300

    Update App function

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -6,33 +6,48 @@ use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
 use crate::routes::matches_route::*;
-use crate::routes::ranking_route::Ranking;
+use crate::routes::login_route::*;
+use crate::routes::register_route::*;
+use crate::services::bets_service::Bet;
 
 
 #[component]
 pub fn App() -> impl IntoView {
     provide_meta_context();
     let (is_routing, set_is_routing) = create_signal(false);
-
+    println!("xd");
     view! {
         // <Stylesheet id="leptos" href="/pkg/hackernews.css"/>
         <Link rel="shortcut icon" type_="image/ico" href="/favicon.ico"/>
         <Meta name="description" content="Leptos implementation of a HackerNews demo."/>
         // adding `set_is_routing` causes the router to wait for async data to load on new pages
         <Router set_is_routing>
-            // shows a progress bar while async data are loading
             <div class="routing-progress">
                 <RoutingProgress is_routing max_time=std::time::Duration::from_millis(250)/>
             </div>
-            //<Nav />
+            <nav>
+            /* ... */
+            </nav>
             <main>
                 <Routes>
-                    <Route path=":match/:id" view=Match/>
-                    <Route path=":matches?" view=Matches/>
-                    <Route path=":ranking?" view=Ranking/>
+                    <Route path="/" view=Home/>
+                    <Route path="/auth/login" view=Login/>
+                    <Route path="/auth/register" view=Register/>
+                    <Route path="/matches" view=Matches/>
+                    <Route path="/matches/:id" view=Match/>
+                    <Route path="/*any" view=|| view! { <h1>"Not Found"</h1> }/>
                 </Routes>
             </main>
         </Router>
+    }
+}
+
+#[component]
+pub fn Home() -> impl IntoView {
+    view! {
+        <h1>
+            Home
+        </h1>
     }
 }
 
