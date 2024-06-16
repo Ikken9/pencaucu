ecdac1d1c0d5de7d33f81e2bd8b1a1d2c30b0b10
ikken <pierosaucedo3@gmail.com>
2024-06-16 01:17:28 -0300

    Update match service

--- frontend/src/services/match_service.rs
+++ frontend/src/services/match_service.rs
@@ -1,4 +1,4 @@
-use reqwest::{Client, get};
+use reqwest::Client;
 use crate::models::match_model::Match;
 
 pub async fn get_matches() -> Result<Vec<Match>, reqwest::Error> {
@@ -13,8 +13,8 @@ pub async fn get_matches() -> Result<Vec<Match>, reqwest::Error> {
 }
 
 pub async fn get_match(id: &str) -> Result<Match, reqwest::Error> {
-    let response: Match = Client::new().
-        get(format!("http://localhost:8080/matches/{}", id))
+    let response: Match = Client::new()
+        .get(format!("http://localhost:8080/matches/{}", id))
         .send()
         .await?
         .json()
