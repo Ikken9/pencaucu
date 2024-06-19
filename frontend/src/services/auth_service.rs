70ddd5f05bd778fb1439fc4c9aa802cbf33ae0a9
ikken <pierosaucedo3@gmail.com>
2024-06-19 17:12:29 -0300

    Update services

--- frontend/src/services/auth_service.rs
+++ frontend/src/services/auth_service.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use leptos::logging::error;
 use leptos::tracing::info;
 use reqwest::{Client, Response};
-use crate::models::auth_model::{EmailAddress, Password, Username};
+use crate::models::auth_model::{Career, EmailAddress, Password, Username};
 
 pub async fn login(email: EmailAddress, password: Password) -> Result<Response, reqwest::Error> {
     let mut map = HashMap::new();
@@ -40,10 +40,11 @@ pub async fn login(email: EmailAddress, password: Password) -> Result<Response, 
     }
 }
 
-pub async fn register(username: Username, email_address: EmailAddress, password: Password) -> Result<Response, reqwest::Error> {
+pub async fn register(username: Username, email_address: EmailAddress, career: Career, password: Password) -> Result<Response, reqwest::Error> {
     let mut map = HashMap::new();
-    map.insert("name", username.to_string());
+    map.insert("username", username.to_string());
     map.insert("email", email_address.to_string());
+    map.insert("career", career.to_string());
     map.insert("password", password.to_string());
 
     let client = Client::new();
@@ -55,21 +56,8 @@ pub async fn register(username: Username, email_address: EmailAddress, password:
 
     match res {
         Ok(response) => {
-            match response.text().await {
-                Ok(text) => {
-                    info!("Register response: {}", text);
-                    // Rebuild the response if necessary (e.g., if you need to inspect it elsewhere)
-                    let rebuilt_response = client.post("http://localhost:8080/auth/register")
-                        .json(&map)
-                        .send()
-                        .await?;
-                    Ok(rebuilt_response)
-                },
-                Err(e) => {
-                    error!("Failed to read response text: {}", e);
-                    Err(e)
-                }
-            }
+            info!("Response {:?}", response);
+            Ok(response)
         }
         Err(e) => {
             error!("Register request failed: {}", e);
