78cc44ce5b86f22af7e5b3cbb6273e2060a55015
ikken <pierosaucedo3@gmail.com>
2024-06-18 23:01:15 -0300

    Add Kanit font support, add custom colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
     content: ["./index.html", "./src/**/*.rs"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'card-container-gray': '#151826',
+                'primary-gray-1': '#282D42',
+                'primary-gray-2': '#353A57',
+                'secondary-gray': '#353A57',
+                'secondary-gray-2': '#4D5481'
+            }
+        },
+        fontFamily: {
+            kanit: ['Kanit', 'sans-serif'],
+        },
     },
     plugins: [
         // ...
