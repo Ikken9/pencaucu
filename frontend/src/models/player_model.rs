91df248d7335fc6c6f20b4b631d2b8a90d612269
ikken <pierosaucedo3@gmail.com>
2024-06-18 23:05:44 -0300

    Update player model, implements Display trait

--- frontend/src/models/player_model.rs
+++ frontend/src/models/player_model.rs
@@ -1,7 +1,14 @@
+use std::fmt::{Display, Formatter};
 use serde::{Deserialize, Serialize};
 
 #[derive(Default, Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Player {
     pub(crate) username: String,
     pub(crate) points: u64
+}
+
+impl Display for Player {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}, {}", self.username, self.points)
+    }
 }
