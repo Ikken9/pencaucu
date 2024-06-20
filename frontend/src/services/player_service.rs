3a0fd8f74e8fb7df6e5b8f875b61686b04c10ba1
ikken <pierosaucedo3@gmail.com>
2024-06-20 13:23:00 -0300

    Update player service to properly make authorized requests and use of token

--- frontend/src/services/player_service.rs
+++ frontend/src/services/player_service.rs
@@ -2,7 +2,7 @@ use reqwest::Client;
 use crate::models::player_model::Player;
 
 pub async fn get_players() -> Result<Vec<Player>, reqwest::Error> {
-    let token = web_sys::window().unwrap().session_storage().unwrap().unwrap().get_item("token").unwrap_or(None);
+    let token = web_sys::window().unwrap().local_storage().unwrap().unwrap().get_item("token").unwrap();
     let client = Client::new();
 
     let req_builder = client.get("http://localhost:8080/players");
