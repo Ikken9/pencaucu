65407029709d2ec557233f406a0baf8b15f48002
ikken <pierosaucedo3@gmail.com>
2024-06-22 18:18:18 -0300

    Update stage model

--- frontend/src/models/stage_model.rs
+++ frontend/src/models/stage_model.rs
@@ -2,8 +2,5 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Default, Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct KnockoutStage {
-    pub(crate) id: u32,
-
-    #[serde(rename = "stageName")]
     pub(crate) name: String
 }
