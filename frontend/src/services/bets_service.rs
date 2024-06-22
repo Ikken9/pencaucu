9aba7127d2dff21b85822f2d63d46b131cf368f5
ikken <pierosaucedo3@gmail.com>
2024-06-22 15:16:31 -0300

    Update bets service

--- frontend/src/services/bets_service.rs
+++ frontend/src/services/bets_service.rs
@@ -1,26 +1,28 @@
+use leptos::logging::log;
 use reqwest::Client;
+use crate::models::bet_model::Bet;
 
-pub async fn make_bet(match_id: &str) -> Result<(), reqwest::Error> {
+pub async fn make_bet(player_email: &str, match_id: &u64) -> Result<Bet, reqwest::Error> {
     let token = web_sys::window().unwrap().local_storage().unwrap().unwrap().get_item("token").unwrap();
     let client = Client::new();
 
-    let url = format!("http://localhost:8080/matches/{}", match_id);
-    let request = client.post(&url);
-    let request = if let Some(token) = token {
-        request.bearer_auth(token)
+    let url = format!("http://localhost:8080/bets/{}/{}", player_email, match_id);
+    let req_builder = client.get(&url);
+
+    let req = if let Some(token) = token {
+        req_builder.bearer_auth(token)
     } else {
-        request
+        req_builder
     };
 
-    let response = request.send().await?;
-    if response.status().is_success() {
-        log!("Bet request successful.");
-        Ok(())
+    let res = req.send().await?;
+
+    if res.status().is_success() {
+        log!("Ok!");
+        let bet = res.json::<Bet>().await?;
+        Ok(bet)
     } else {
-        log!("Bet request failed with status: {}", response.status());
-        Err(reqwest::Error::new(
-            reqwest::StatusCode::INTERNAL_SERVER_ERROR,
-            "Bet request failed",
-        ))
+        log!("Err!");
+        Err(res.error_for_status().unwrap_err())
     }
 }
