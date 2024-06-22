704f01d6b39f1c2eb45dfb3008832bdbbc50096e
ikken <pierosaucedo3@gmail.com>
2024-06-22 16:16:40 -0300

    Update bets page

--- frontend/src/routes/bets_route.rs
+++ frontend/src/routes/bets_route.rs
@@ -6,7 +6,6 @@ use crate::services::match_service::u64_to_date;
 #[component]
 pub fn Bet() -> impl IntoView {
     let params = use_params_map();
-
     let match_data = create_resource(
         move || params.get().get("match-id").cloned().unwrap_or_default(),
         move |id| async move {
