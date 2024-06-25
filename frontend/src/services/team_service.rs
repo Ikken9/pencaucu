a12e60d5ca71e2d7636e516b4a4f79a082e8591c
ikken <pierosaucedo3@gmail.com>
2024-06-25 15:15:57 -0300

    Update team service

--- frontend/src/services/team_service.rs
+++ frontend/src/services/team_service.rs
@@ -1,11 +1,10 @@
 use reqwest::Client;
-use crate::models::team_model::Team;
 
-pub async fn get_teams() -> Result<Vec<Team>, reqwest::Error> {
+pub async fn get_teams_names() -> Result<Vec<String>, reqwest::Error> {
     let token = web_sys::window().unwrap().session_storage().unwrap().unwrap().get_item("token").unwrap_or(None);
     let client = Client::new();
 
-    let req_builder = client.get("http://localhost:8080/teams");
+    let req_builder = client.get("http://localhost:8080/teams/names");
 
     let req = if let Some(token) = token {
         req_builder.bearer_auth(token)
@@ -16,7 +15,7 @@ pub async fn get_teams() -> Result<Vec<Team>, reqwest::Error> {
     let res = req.send().await?;
 
     if res.status().is_success() {
-        let teams = res.json::<Vec<Team>>().await?;
+        let teams = res.json::<Vec<String>>().await?;
         Ok(teams)
     } else {
         Err(res.error_for_status().unwrap_err())
