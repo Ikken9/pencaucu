597d5d60c3a4f263b7195eea7f7679e564ebc4b3
ikken <pierosaucedo3@gmail.com>
2024-06-25 15:16:21 -0300

    Update register

--- frontend/src/services/register_service.rs
+++ frontend/src/services/register_service.rs
@@ -3,11 +3,13 @@ use reqwest::{Client, Response};
 use crate::models::auth_model::{EmailAddress, Password, Username};
 use crate::models::career_model::Career;
 
-pub async fn register(username: Username, email_address: EmailAddress, career: Career, password: Password) -> Result<Response, reqwest::Error> {
+pub async fn register(username: Username, email_address: EmailAddress, career: Career, champion: String, subchampion: String, password: Password) -> Result<Response, reqwest::Error> {
     let mut map = HashMap::new();
     map.insert("username", username.to_string());
     map.insert("email", email_address.to_string());
     map.insert("career", career.to_string());
+    map.insert("champion", champion);
+    map.insert("subchampion", subchampion);
     map.insert("password", password.to_string());
 
     let client = Client::new();
