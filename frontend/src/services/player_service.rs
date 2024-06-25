d10b0fc22158a4baffbd65dcf8c874193a4a3df0
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2024-06-25 15:52:00 -0300

    profile screen fix in request

--- frontend/src/services/player_service.rs
+++ frontend/src/services/player_service.rs
@@ -24,7 +24,7 @@ pub async fn get_players() -> Result<Vec<Player>, reqwest::Error> {
 }
 
 pub async fn get_player(email: &str) -> Result<Player, reqwest::Error> {
-    let token = web_sys::window().unwrap().session_storage().unwrap().unwrap().get_item("token").unwrap_or(None);
+    let token = web_sys::window().unwrap().local_storage().unwrap().unwrap().get_item("token").unwrap();
     let client = Client::new();
 
     let req_builder = client.get(format!("http://localhost:8080/players/{}", email));
