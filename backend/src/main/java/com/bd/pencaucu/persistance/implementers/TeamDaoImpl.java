6351cb91fea293e5c7ee40762db0bd3908551089
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2024-06-25 15:54:59 -0300

    team querys implement

--- backend/src/main/java/com/bd/pencaucu/persistance/implementers/TeamDaoImpl.java
+++ backend/src/main/java/com/bd/pencaucu/persistance/implementers/TeamDaoImpl.java
@@ -1,5 +1,6 @@
 package com.bd.pencaucu.persistance.implementers;
 
+import com.bd.pencaucu.mappers.models.TeamMapper;
 import com.bd.pencaucu.mappers.models.TeamNameMapper;
 import com.bd.pencaucu.models.Team;
 import com.bd.pencaucu.persistance.interfaces.TeamDao;
@@ -17,12 +18,21 @@ public class TeamDaoImpl implements TeamDao {
 
     @Override
     public Team findById(String id) {
+        String sql = "SELECT name, group_letter, flag_image FROM Teams WHERE name = ?";
+        List<Team> teams = jdbcTemplate.query(sql, new TeamMapper(), id);
+
+        if (!teams.isEmpty()) {
+            return teams.get(0);
+        }
+
         return null;
     }
 
     @Override
     public List<Team> findAll() {
-        return List.of();
+        String sql = "SELECT name, group_letter, flag_image FROM Teams";
+
+        return jdbcTemplate.query(sql, new TeamMapper());
     }
 
     @Override
@@ -34,16 +44,24 @@ public class TeamDaoImpl implements TeamDao {
 
     @Override
     public void save(Team team) {
+        String sql = "INSERT INTO Teams (name, group_letter, flag_image) VALUES (?, ?, ?)";
 
+        jdbcTemplate.update(sql,
+                team.getName(),
+                team.getGroupLetter(),
+                team.getPictureUrl());
     }
 
     @Override
     public void update(Team team) {
+        String sql = "UPDATE Teams SET group_letter = ?, flag_image = ? WHERE name = ?";
 
+        jdbcTemplate.update(sql, team.getGroupLetter(), team.getPictureUrl(), team.getName());
     }
 
     @Override
     public void delete(String id) {
-
+        String sql = "DELETE FROM Teams WHERE name = ?";
+        jdbcTemplate.update(sql, id);
     }
 }
