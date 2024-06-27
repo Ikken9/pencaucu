ab3d63f1ae17698d00046c14836d0cc681ec83aa
MartinCaraballo <martin.caraballo@correo.ucu.edu.uy>
2024-06-27 12:03:01 -0300

    init.sql updated

--- backend/init.sql
+++ backend/init.sql
@@ -3,99 +3,99 @@ CREATE DATABASE IF NOT EXISTS pencaucu_db;
 USE pencaucu_db;
 
 CREATE TABLE IF NOT EXISTS Careers(
-    career_name VARCHAR(64) NOT NULL,
-    PRIMARY KEY (career_name)
+                                      career_name VARCHAR(64) NOT NULL,
+                                      PRIMARY KEY (career_name)
 );
 
 CREATE TABLE IF NOT EXISTS Users(
-    email VARCHAR(64) NOT NULL,
-    username VARCHAR(64) UNIQUE NOT NULL,
-    PRIMARY KEY (email)
+                                    email VARCHAR(64) NOT NULL,
+                                    username VARCHAR(64) UNIQUE NOT NULL,
+                                    PRIMARY KEY (email)
 );
 
 CREATE TABLE IF NOT EXISTS Logins(
-    user_email VARCHAR(64) NOT NULL,
-    password VARCHAR(256) NOT NULL,
-    PRIMARY KEY (user_email),
-    FOREIGN KEY (user_email) REFERENCES Users(email)
+                                     user_email VARCHAR(64) NOT NULL,
+                                     password VARCHAR(256) NOT NULL,
+                                     PRIMARY KEY (user_email),
+                                     FOREIGN KEY (user_email) REFERENCES Users(email)
 );
 
 CREATE TABLE IF NOT EXISTS Players(
-    player_email VARCHAR(64) NOT NULL,
-    career_name VARCHAR(64) NOT NULL,
-    profile_picture VARCHAR(256),
-    PRIMARY KEY (player_email),
-    FOREIGN KEY (player_email) REFERENCES Users(email),
-    FOREIGN KEY (career_name) REFERENCES Careers(career_name)
+                                      player_email VARCHAR(64) NOT NULL,
+                                      career_name VARCHAR(64) NOT NULL,
+                                      profile_picture VARCHAR(256),
+                                      PRIMARY KEY (player_email),
+                                      FOREIGN KEY (player_email) REFERENCES Users(email),
+                                      FOREIGN KEY (career_name) REFERENCES Careers(career_name)
 );
 
 CREATE TABLE IF NOT EXISTS Admins(
-    admin_email VARCHAR(64) NOT NULL,
-    PRIMARY KEY (admin_email),
-    FOREIGN KEY (admin_email) REFERENCES Users(email)
+                                     admin_email VARCHAR(64) NOT NULL,
+                                     PRIMARY KEY (admin_email),
+                                     FOREIGN KEY (admin_email) REFERENCES Users(email)
 );
 
 CREATE TABLE IF NOT EXISTS Knockout_Stage(
-    name VARCHAR(64) NOT NULL,
-    PRIMARY KEY (name)
+                                             name VARCHAR(64) NOT NULL,
+                                             PRIMARY KEY (name)
 );
 
 CREATE TABLE IF NOT EXISTS Teams(
-    name VARCHAR(64) NOT NULL,
-    group_letter CHAR NOT NULL,
-    flag_image VARCHAR(256),
-    PRIMARY KEY (name)
+                                    name VARCHAR(64) NOT NULL,
+                                    group_letter CHAR NOT NULL,
+                                    flag_image VARCHAR(256),
+                                    PRIMARY KEY (name)
 );
 
 CREATE TABLE IF NOT EXISTS Matches(
-    id INT AUTO_INCREMENT,
-    date DATETIME NOT NULL,
-    knockout_stage VARCHAR(64),
-    stadium_id VARCHAR(20) NOT NULL,
-    team_name VARCHAR(64) NOT NULL,
-    faced_team_name VARCHAR(64) NOT NULL,
-    admin_email VARCHAR(64) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (knockout_stage) REFERENCES Knockout_Stage(name),
-    FOREIGN KEY (team_name) REFERENCES Teams(name),
-    FOREIGN KEY (faced_team_name) REFERENCES Teams(name),
-    FOREIGN KEY (admin_email) REFERENCES Admins(admin_email)
+                                      id INT AUTO_INCREMENT,
+                                      date DATETIME NOT NULL UNIQUE,
+                                      knockout_stage VARCHAR(64),
+                                      stadium_id VARCHAR(20) NOT NULL,
+                                      team_name VARCHAR(64) NOT NULL,
+                                      faced_team_name VARCHAR(64) NOT NULL,
+                                      admin_email VARCHAR(64) NOT NULL,
+                                      PRIMARY KEY (id),
+                                      FOREIGN KEY (knockout_stage) REFERENCES Knockout_Stage(name),
+                                      FOREIGN KEY (team_name) REFERENCES Teams(name),
+                                      FOREIGN KEY (faced_team_name) REFERENCES Teams(name),
+                                      FOREIGN KEY (admin_email) REFERENCES Admins(admin_email)
 );
 
 CREATE TABLE IF NOT EXISTS Results(
-    match_id INT NOT NULL,
-    team_score INT,
-    faced_team_score INT,
-    PRIMARY KEY (match_id),
-    FOREIGN KEY (match_id) REFERENCES Matches(id)
+                                      match_id INT NOT NULL,
+                                      team_score INT,
+                                      faced_team_score INT,
+                                      PRIMARY KEY (match_id),
+                                      FOREIGN KEY (match_id) REFERENCES Matches(id)
 );
 
 CREATE TABLE IF NOT EXISTS Stadiums(
-    id VARCHAR(20) NOT NULL,
-    country VARCHAR(64) NOT NULL,
-    state VARCHAR(64) NOT NULL,
-    city VARCHAR(64) NOT NULL,
-    name VARCHAR(64) NOT NULL,
-    PRIMARY KEY (id)
+                                       id VARCHAR(20) NOT NULL,
+                                       country VARCHAR(64) NOT NULL,
+                                       state VARCHAR(64) NOT NULL,
+                                       city VARCHAR(64) NOT NULL,
+                                       name VARCHAR(64) NOT NULL,
+                                       PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS Bets(
-    player_email VARCHAR(64) NOT NULL,
-    match_id INT NOT NULL,
-    team_score INT NOT NULL,
-    faced_team_score INT NOT NULL,
-    PRIMARY KEY (player_email, match_id),
-    FOREIGN KEY (player_email) REFERENCES Players(player_email),
-    FOREIGN KEY (match_id) REFERENCES Matches(id)
+                                   player_email VARCHAR(64) NOT NULL,
+                                   match_id INT NOT NULL,
+                                   team_score INT NOT NULL,
+                                   faced_team_score INT NOT NULL,
+                                   PRIMARY KEY (player_email, match_id),
+                                   FOREIGN KEY (player_email) REFERENCES Players(player_email),
+                                   FOREIGN KEY (match_id) REFERENCES Matches(id)
 );
 
 CREATE TABLE IF NOT EXISTS Player_Ranks(
-    player_email VARCHAR(64) NOT NULL,
-    team_name VARCHAR(64) NOT NULL,
-    final_position INT NOT NULL,
-    PRIMARY KEY (player_email, team_name),
-    FOREIGN KEY (player_email) REFERENCES Players(player_email),
-    FOREIGN KEY (team_name) REFERENCES Teams(name)
+                                           player_email VARCHAR(64) NOT NULL,
+                                           team_name VARCHAR(64) NOT NULL,
+                                           final_position INT NOT NULL,
+                                           PRIMARY KEY (player_email, team_name),
+                                           FOREIGN KEY (player_email) REFERENCES Players(player_email),
+                                           FOREIGN KEY (team_name) REFERENCES Teams(name)
 );
 
 -- START DUMMY INSERTS
@@ -223,9 +223,7 @@ INSERT IGNORE INTO Knockout_Stage(name)
 VALUES ('Final');
 
 -- MATCHES EXAMPLE DATA INSERT
-
 -- GRUPO A
-
 INSERT IGNORE INTO Matches(date, knockout_stage, stadium_id, team_name, faced_team_name, admin_email)
 VALUES ('2024-06-20 21:00:00', 'Fase de Grupos', 'MBS-ATL-EEUU', 'Argentina', 'Canada', 'martin.caraballo@correo.ucu.edu.uy');
 
@@ -244,8 +242,8 @@ VALUES ('2024-06-29 21:00:00', 'Fase de Grupos', 'HRS-MIA-EEUU', 'Argentina', 'P
 INSERT IGNORE INTO Matches(date, knockout_stage, stadium_id, team_name, faced_team_name, admin_email)
 VALUES ('2024-06-29 21:00:00', 'Fase de Grupos', 'ICS-OAO-EEUU', 'Canada', 'Chile', 'martin.caraballo@correo.ucu.edu.uy');
 
---GRUPO B
 
+-- GRUPO B
 INSERT IGNORE INTO Matches(date, knockout_stage, stadium_id, team_name, faced_team_name, admin_email)
 VALUES ('2024-06-22 19:00:00', 'Fase de Grupos', 'LS-SC-EEUU', 'Ecuador', 'Venezuela', 'martin.caraballo@correo.ucu.edu.uy');
 
@@ -264,8 +262,8 @@ VALUES ('2024-06-30 21:00:00', 'Fase de Grupos', 'SFS-GDE-EEUU', 'Mexico', 'Ecua
 INSERT IGNORE INTO Matches(date, knockout_stage, stadium_id, team_name, faced_team_name, admin_email)
 VALUES ('2024-06-30 21:00:00', 'Fase de Grupos', 'Q2S-AST-EEUU', 'Jamaica', 'Venezuela', 'martin.caraballo@correo.ucu.edu.uy');
 
---GRUPO C
 
+-- GRUPO C
 INSERT IGNORE INTO Matches(date, knockout_stage, stadium_id, team_name, faced_team_name, admin_email)
 VALUES ('2024-06-23 19:00:00', 'Fase de Grupos', 'ATS-ARL-EEUU', 'USA', 'Bolivia', 'martin.caraballo@correo.ucu.edu.uy');
 
@@ -284,8 +282,8 @@ VALUES ('2024-07-01 22:00:00', 'Fase de Grupos', 'AHS-KSC-EEUU', 'USA', 'Uruguay
 INSERT IGNORE INTO Matches(date, knockout_stage, stadium_id, team_name, faced_team_name, admin_email)
 VALUES ('2024-07-01 22:00:00', 'Fase de Grupos', 'ICS-OAO-EEUU', 'Bolivia', 'Panama', 'martin.caraballo@correo.ucu.edu.uy');
 
---GRUPO D
 
+-- GRUPO D
 INSERT IGNORE INTO Matches(date, knockout_stage, stadium_id, team_name, faced_team_name, admin_email)
 VALUES ('2024-06-24 19:00:00', 'Fase de Grupos', 'NRG-HSN-EEUU', 'Colombia', 'Paraguay', 'martin.caraballo@correo.ucu.edu.uy');
 
@@ -305,7 +303,7 @@ INSERT IGNORE INTO Matches(date, knockout_stage, stadium_id, team_name, faced_te
 VALUES ('2024-07-02 22:00:00', 'Fase de Grupos', 'Q2S-AST-EEUU', 'Costa Rica', 'Paraguay', 'martin.caraballo@correo.ucu.edu.uy');
 
 
-    -- RESULTS EXAMPLE DATA INSERT
+-- RESULTS EXAMPLE DATA INSERT
 INSERT IGNORE INTO Results(match_id, team_score, faced_team_score)
 VALUES (1, 2, 0);
 
